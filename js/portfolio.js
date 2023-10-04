b7d6d7061034b997dec6559142ce16a610e60341
Anthony Hurtado <anhurtado@outlook.com>
2023-10-03 20:51:37 -0400

    fix: updated way to get json data

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,37 @@
-const fnLoaded = async () => {
-  // Request
-  const response = await fetch('./json/portfolio.json');
-  const data = await response.json();
+// Request
+const portfolioRequest = new XMLHttpRequest();
+portfolioRequest.open('GET', './json/portfolio.json', true);
 
-  // Project elements
-  let projectElements = '';
-  for (const item of data.projects) {
-    projectElements += `
-      <section id="item">
-        <img src="${item.image}" alt="${item.title}">
-        <section id="info">
-          <a href="${item.link}" target="_blank"><h2>${item.title}</h2></a>
-          <p>${item.subtitle}</p>
-          <p>${item.tech}</p>
+// Callback
+portfolioRequest.onload = function() {
+  if (portfolioRequest.status >= 200 && portfolioRequest.status < 400) {
+    // Conversion to JSON
+    const data = JSON.parse(portfolioRequest.responseText);
+
+    // Project elements
+    let projectElements = '';
+    for (const item of data.projects) {
+      projectElements += `
+        <section id="item">
+          <img src="${item.image}" alt="${item.title}">
+          <section id="info">
+            <a href="${item.link}" target="_blank"><h2>${item.title}</h2></a>
+            <p>${item.subtitle}</p>
+            <p>${item.tech}</p>
+          </section>
         </section>
-      </section>
-    `;
+      `;
+    }
+    document.getElementById('projects').innerHTML = projectElements;
+  } else {
+    console.log('Error al cargar el archivo JSON');
   }
-  document.getElementById('projects').innerHTML = projectElements;
 };
-document.addEventListener('DOMContentLoaded', fnLoaded);
+
+// Error handler
+portfolioRequest.onerror = function() {
+  console.log('Error al intentar conectarse con el archivo JSON');
+};
+
+// Call request
+portfolioRequest.send();
