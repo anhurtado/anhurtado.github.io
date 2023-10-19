62542743625779c4072ac007070065736a863dcc
Anthony Hurtado <anhurtado@outlook.com>
2023-10-19 16:58:57 -0400

    feat: design changes

--- components/SocialNetwork/index.tsx
+++ components/SocialNetwork/index.tsx
@@ -4,15 +4,15 @@ import { socialNetworkData } from '@/data/social-network.data'
 
 export default function SocialNetwork() {
   return (
-    <section className="flex flex-wrap gap-4">
+    <section className="grid grid-cols-7 gap-6">
       {socialNetworkData.networks.map((item) => (
         <a href={item.url} target="_blank" key={item.alt}>
           <Image
             src={`/icons/${item.icon}`}
             alt={item.alt}
             title={item.alt}
-            width={25}
-            height={25} />
+            width={30}
+            height={30} />
         </a>
       ))}
     </section>
